244cc22f33ae19601088cb6f11aa336dfd5351b5
Brent Houston <brentjustinhouston@gmail.com>
2023-08-25 11:52:53 -0500

    animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-
-
-export default function Home () {
-    return(
-        <div className="container hero">
-
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      <div className="container hero">
+      <div><div className="ball"></div></div>
+        
+        <div className="ball1"></div>
+        <div><div className="ball2"></div></div>
+        
+        
+        
+      </div>
+      
+    </div>
+  );
 }
