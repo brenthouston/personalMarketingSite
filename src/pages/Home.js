0644fb201d2583632c9f4eaf025f63adeaee62a9
Brent Houston <brentjustinhouston@gmail.com>
2023-08-29 22:48:00 -0500

    hero animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,18 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <div className="container hero">
-      <div><div className="ball"></div></div>
-        
-        <div className="ball1"></div>
-        <div><div className="ball2"></div></div>
-        
-        
-        
-      </div>
-      
+    <div className="container">
+     
+        <div className="container hero">
+            <div className="allBalls">
+        <div className="ball"></div>
+        <div className="ball1">  <article className="welcomeText">
+        <h1 style={{tranistion:"10ms"}}>Welcome, to my page. Let's create and captivate together!</h1>
+      </article></div>
+        <div className="ball2"></div></div></div>
+     
+    
+    
     </div>
   );
 }
